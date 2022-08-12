73257b1004ccf327d1382d0e9a8a6d689b715491
Fredrik Orderud <fredrik.orderud@ge.com>
2022-08-12 14:34:59 +0200

    Extend certificate lifetimes to 10 years
    Done to ease testing.
    
    Chrome does usually have a max 1year lifetime for web server certificates as documented on https://chromium.googlesource.com/chromium/src/+/HEAD/net/docs/certificate_lifetimes.md . However, there seem to be an exception for locally-operated CAs that have been manually configured.

--- TestCertificates/GenerateCertificates.bat
+++ TestCertificates/GenerateCertificates.bat
@@ -5,7 +5,7 @@
 set "PATH=%PATH%;C:\Program Files\Git\usr\bin"
 
 :: Generate root certificate
-makecert.exe -r -n "CN=TestRootCertificate" -pe -sv TestRootCertificate.pvk -a sha256 -len 2048 -b 01/01/2022 -e 01/01/2023 -cy authority TestRootCertificate.cer
+makecert.exe -r -n "CN=TestRootCertificate" -pe -sv TestRootCertificate.pvk -a sha256 -len 2048 -b 01/01/2022 -e 01/01/2032 -cy authority TestRootCertificate.cer
 :: Bundle to .pfx
 pvk2pfx.exe -pvk TestRootCertificate.pvk -spc TestRootCertificate.cer -pfx TestRootCertificate.pfx
 :: Convert to PEM for OpenSSL compatibility
@@ -15,7 +15,7 @@ openssl.exe pkcs12 -in TestRootCertificate.pfx -out TestRootCertificate.pem -nod
 :: Generate client certificate from root certificate
 :: The enhanced key usage (EKU) OID for clientAuth is 1.3.6.1.5.5.7.3.2
 :: The enhanced key usage (EKU) OID for codeSigning is 1.3.6.1.5.5.7.3.3
-makecert.exe -ic TestRootCertificate.cer -iv TestRootCertificate.pvk -pe -sv ClientCert.pvk -a sha256 -n "CN=ClientCert" -len 2048 -b 01/01/2022 -e 01/01/2023 -sky exchange ClientCert.cer -eku 1.3.6.1.5.5.7.3.2,1.3.6.1.5.5.7.3.3
+makecert.exe -ic TestRootCertificate.cer -iv TestRootCertificate.pvk -pe -sv ClientCert.pvk -a sha256 -n "CN=ClientCert" -len 2048 -b 01/01/2022 -e 01/01/2032 -sky exchange ClientCert.cer -eku 1.3.6.1.5.5.7.3.2,1.3.6.1.5.5.7.3.3
 :: Bundle to .pfx
 pvk2pfx.exe -pvk ClientCert.pvk -spc ClientCert.cer -pfx ClientCert.pfx
 :: Convert to PEM for OpenSSL compatibility
@@ -30,4 +30,4 @@ openssl.exe pkcs12 -in ClientCert.pfx -out ClientCert.pem -nodes
 openssl.exe req -newkey rsa:2048 -subj "/CN=localhost" -keyout localhost.key -out localhost.csr -nodes
 :: Sign certificate with root certificate
 :: Doc: https://www.openssl.org/docs/manmaster/man1/openssl-x509.html
-openssl.exe x509 -req -extfile openssl.ini -extensions MyCustomExtensions -CA TestRootCertificate.pem -CAcreateserial -in localhost.csr -out localhost.crt -days 365
+openssl.exe x509 -req -extfile openssl.ini -extensions MyCustomExtensions -CA TestRootCertificate.pem -CAcreateserial -in localhost.csr -out localhost.crt -days 3650
