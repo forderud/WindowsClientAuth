131f4f4411d19e6169670aaa751c89d73b7b4fb5
Fredrik Orderud <fredrik.orderud@ge.com>
2022-09-21 11:57:48 +0200

    My Python sample into a separate project.

--- WebClientPy/WebClient.py
+++ WebClientPy/WebClient.py
@@ -7,7 +7,7 @@ if len(sys.argv) > 1:
 
 # file paths relative to this script
 # TODO: Load certificate from Windows certificate store instead of file (see https://stackoverflow.com/questions/55229786/loading-certificates-into-ssl-with-certs-store-not-file-path-with-python)
-CERT_FILE = os.path.join(os.path.dirname(__file__), 'TestCertificates\\ClientCert.pem')
+CERT_FILE = os.path.join(os.path.dirname(__file__), '..\\TestCertificates\\ClientCert.pem')
 
 # configure client certificate
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
