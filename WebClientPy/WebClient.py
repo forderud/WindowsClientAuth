f25c6342ded11ce87c47945cf974dd6c9e5f6f01
Fredrik Orderud <fredrik.orderud@ge.com>
2022-09-21 12:17:25 +0200

    Python: Switch to accessing certificates from Windows certificate store instead of files.
    WARNING: Does NOT work - at least not yet! The store seem to be loaded, but no client cert is actually used for client auth.

--- WebClientPy/WebClient.py
+++ WebClientPy/WebClient.py
@@ -5,14 +5,11 @@ if len(sys.argv) > 1:
     host_port = sys.argv[1].split(":")
     hostname = (host_port[0], int(host_port[1]))
 
-# file paths relative to this script
-# TODO: Load certificate from Windows certificate store instead of file (see https://stackoverflow.com/questions/55229786/loading-certificates-into-ssl-with-certs-store-not-file-path-with-python)
-CERT_FILE = os.path.join(os.path.dirname(__file__), '..\\TestCertificates\\ClientCert.pem')
-
 # configure client certificate
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.load_default_certs()
-context.load_cert_chain(certfile=CERT_FILE)
+certs = context._load_windows_store_certs("My", ssl.Purpose.CLIENT_AUTH)
+print(certs)
 
 # submit HTTP request
 conn = http.client.HTTPSConnection(hostname[0], port=hostname[1], context=context)
