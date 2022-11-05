5d8b2d5df293464952346a1f6bb01f120ceaf674
Fredrik Orderud <forderud@gmail.com>
2022-11-05 16:42:08 +0100

    Use com_array<T> directly.

--- WebClientCpp/Main.cpp
+++ WebClientCpp/Main.cpp
@@ -4,17 +4,10 @@ using namespace winrt;
 using namespace Windows::Security::Cryptography::Certificates;
 
 void CertAccessWin32();
-void HttpGetMSXML6(std::wstring url, std::vector<uint8_t> thumbprint);
+void HttpGetMSXML6(std::wstring url, const com_array<uint8_t>& thumbprint);
 Certificate GetFirstClientAuthCert();
 void HttpGetWinRT(std::wstring url, Certificate clientCert);
 
-
-static std::vector<uint8_t> ToVector(const com_array<uint8_t> &input) {
-    std::vector<uint8_t> result(input.size());
-    for (uint32_t i = 0; i < input.size(); ++i)
-        result[i] = input[i];
-    return result;
-}
 
 int wmain(int argc, wchar_t* argv[]) {
     init_apartment();
@@ -34,7 +27,7 @@ int wmain(int argc, wchar_t* argv[]) {
 
         std::wcout << "HTTP request using MSXML6:\n";
         com_array<uint8_t> thumbprint = clientCert.GetHashValue(); // shown as "Thumbprint" in Windows cert. manager
-        HttpGetMSXML6(url, ToVector(thumbprint));
+        HttpGetMSXML6(url, thumbprint);
         
         std::wcout << "\n\nHTTP request using WinRT HttpClient:\n";
         HttpGetWinRT(url, clientCert);
--- WebClientCpp/WebClientMSXML.cpp
+++ WebClientCpp/WebClientMSXML.cpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 
 #pragma comment(lib, "msxml6.lib")
 
@@ -80,11 +79,11 @@ private:
 };
 
 
-void HttpGetMSXML6(std::wstring url, std::vector<uint8_t> thumbprint) {
+void HttpGetMSXML6(std::wstring url, const com_array<uint8_t>& thumbprint) {
     com_ptr<IXMLHTTPRequest3> http;
     check_hresult(CoCreateInstance(CLSID_FreeThreadedXMLHTTP60, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(http.put())));
 
-    check_hresult(http->SetClientCertificate((DWORD)thumbprint.size(), thumbprint.data(), NULL));
+    check_hresult(http->SetClientCertificate(thumbprint.size(), thumbprint.data(), NULL));
 
     com_ptr<HttpRequest3Callback> cb = make_self<HttpRequest3Callback>();
 
