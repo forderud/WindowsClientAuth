396b54cdd3c91741f4279a6da35885bef6d2b459
Fredrik Orderud <forderud@gmail.com>
2022-11-06 20:51:40 +0100

    More OO design.

--- WebClientCpp/CertAccessWin32.cpp
+++ WebClientCpp/CertAccessWin32.cpp
@@ -38,62 +38,92 @@ void OpenCNGKey(const wchar_t* providername, const wchar_t* keyname, DWORD legac
     NCryptFreeObject(provider);
 }
 
-/** CertGetCertificateContextProperty convenience wrapper.
-    Expects the query to either succeed or fail with CRYPT_E_NOT_FOUND. */
-static std::vector<BYTE> CertContextProperty(const CERT_CONTEXT& cert, DWORD prop) {
-    DWORD buffer_len = 0;
-    if (!CertGetCertificateContextProperty(&cert, prop, nullptr, &buffer_len)) {
-        DWORD err = GetLastError();
-        if (err == CRYPT_E_NOT_FOUND)
+
+class Certificate {
+public:
+    Certificate(const CERT_CONTEXT* cert) : m_cert(cert) {
+    }
+
+    operator bool() const {
+        return m_cert;
+    }
+
+    std::wstring Name() const {
+        assert(m_cert);
+        // print certificate name
+        wchar_t buffer[1024] = {};
+        DWORD len = CertNameToStrW(m_cert->dwCertEncodingType, &m_cert->pCertInfo->Subject, CERT_SIMPLE_NAME_STR, buffer, (DWORD)std::size(buffer));
+        assert(len > 0);
+        return buffer;
+    }
+
+    /** Check if a certificate will expire within the next "X" days. */
+    bool WillExpireInDays(int days) {
+        assert(m_cert);
+        assert(m_cert->pCertInfo);
+
+        // get current time in UTC
+        FILETIME currentTime = {};
+        GetSystemTimePreciseAsFileTime(&currentTime);
+
+        // move current time "X" days forward
+        static constexpr uint64_t SECOND = 10000000L; // 100-nanosecond scale
+        (uint64_t&)currentTime += days * 24 * 60 * 60 * SECOND;
+
+        // check if certificate will then have expired
+        LONG diff = CompareFileTime(&m_cert->pCertInfo->NotAfter, &currentTime);
+        return diff < 0;
+    }
+
+    /** CertGetCertificateContextProperty convenience wrapper.
+        Expects the query to either succeed or fail with CRYPT_E_NOT_FOUND. */
+    std::vector<BYTE> ContextProperty(DWORD prop) {
+        assert(m_cert);
+
+        DWORD buffer_len = 0;
+        if (!CertGetCertificateContextProperty(m_cert, prop, nullptr, &buffer_len)) {
+            DWORD err = GetLastError();
+            if (err == CRYPT_E_NOT_FOUND)
+                return {};
+
+            abort();
+        }
+        std::vector<BYTE> buffer(buffer_len, 0);
+        if (!CertGetCertificateContextProperty(m_cert, prop, buffer.data(), &buffer_len)) {
+            DWORD err = GetLastError();
+            err;
+            abort();
+        }
+
+        return buffer;
+    }
+
+    /** CertGetEnhancedKeyUsage convenience wrapper. Returns empty string if no EKU fields are found. */
+    std::vector<std::string> EnhancedKeyUsage(DWORD flags) {
+        assert(m_cert);
+
+        DWORD len = 0;
+        BOOL ok = CertGetEnhancedKeyUsage(m_cert, flags, nullptr, &len);
+        if (!ok || (len == 0))
             return {};
 
-        abort();
+        std::vector<BYTE> eku_buf;
+        eku_buf.resize(len, (BYTE)0);
+        ok = CertGetEnhancedKeyUsage(m_cert, flags, (CERT_ENHKEY_USAGE*)eku_buf.data(), &len);
+        assert(ok);
+
+        auto* eku = (CERT_ENHKEY_USAGE*)eku_buf.data();
+
+        std::vector<std::string> result;
+        for (DWORD i = 0; i < eku->cUsageIdentifier; ++i)
+            result.push_back(eku->rgpszUsageIdentifier[i]);
+
+        return result;
     }
-    std::vector<BYTE> buffer(buffer_len, 0);
-    if (!CertGetCertificateContextProperty(&cert, prop, buffer.data(), &buffer_len)) {
-        DWORD err = GetLastError();
-        err;
-        abort();
-    }
 
-    return buffer;
-}
-
-/** Check if a certificate will expire within the next "X" days. */
-static bool CertWillExpireInDays(CERT_INFO & cert_info, int days) {
-     // get current time in UTC
-    FILETIME currentTime = {};
-    GetSystemTimePreciseAsFileTime(&currentTime);
-
-    // move current time "X" days forward
-    static constexpr uint64_t SECOND = 10000000L; // 100-nanosecond scale
-    (uint64_t&)currentTime += days*24*60*60*SECOND;
-
-    // check if certificate will then have expired
-    LONG diff = CompareFileTime(&cert_info.NotAfter, &currentTime);
-    return diff < 0;
-}
-
-/** CertGetEnhancedKeyUsage convenience wrapper. Returns empty string if no EKU fields are found. */
-static std::vector<std::string> CertEnhancedKeyUsage (const CERT_CONTEXT& cert, DWORD flags) {
-    DWORD len = 0;
-    BOOL ok = CertGetEnhancedKeyUsage(&cert, flags, nullptr, &len);
-    if (!ok || (len == 0))
-        return {};
-
-    std::vector<BYTE> eku_buf;
-    eku_buf.resize(len, (BYTE)0);
-    ok = CertGetEnhancedKeyUsage(&cert, flags, (CERT_ENHKEY_USAGE*)eku_buf.data(), &len);
-    assert(ok);
-
-    auto* eku = (CERT_ENHKEY_USAGE*)eku_buf.data();
-
-    std::vector<std::string> result;
-    for (DWORD i = 0; i < eku->cUsageIdentifier; ++i)
-        result.push_back(eku->rgpszUsageIdentifier[i]);
-    
-    return result;
-}
+private:
+    const CERT_CONTEXT* m_cert = nullptr; //< weak ptr.
+};
 
 class CertStore {
 public:
@@ -122,19 +152,15 @@ private:
 void CertAccessWin32() {
     CertStore store(L"My", true);
 
-    for (const CERT_CONTEXT* cert = store.Next(); cert; cert = store.Next()) {
-        // print certificate name
-        wchar_t buffer[1024] = {};
-        DWORD len = CertNameToStrW(cert->dwCertEncodingType, &cert->pCertInfo->Subject, CERT_SIMPLE_NAME_STR, buffer, (DWORD)std::size(buffer));
-        assert(len > 0);
-        std::wcout << L"Cert: " << buffer << L'\n';
+    for (Certificate cert = store.Next(); cert; cert = store.Next()) {
+        std::wcout << L"Cert: " << cert.Name() << L'\n';
 
-        if (CertWillExpireInDays(*cert->pCertInfo, 31)) {
+        if (cert.WillExpireInDays(31)) {
             std::wcout << L"  Cert will expire within a month.\n";
             continue;
         }
 
-        std::vector<BYTE> thumbprint = CertContextProperty(*cert, CERT_HASH_PROP_ID);
+        std::vector<BYTE> thumbprint = cert.ContextProperty(CERT_HASH_PROP_ID);
         std::wcout << L"  Thumbprint: ";
         for (BYTE elm : thumbprint) {
             wchar_t buffer[3] = {};
@@ -143,11 +169,11 @@ void CertAccessWin32() {
         }
         std::wcout << L'\n';
 
-        std::vector<std::string> ekus = CertEnhancedKeyUsage(*cert, 0);
+        std::vector<std::string> ekus = cert.EnhancedKeyUsage(0);
         for (auto eku : ekus)
             std::cout << "  EKU: " << eku << '\n';
 
-        std::vector<BYTE> prov_buf = CertContextProperty(*cert, CERT_KEY_PROV_INFO_PROP_ID);
+        std::vector<BYTE> prov_buf = cert.ContextProperty(CERT_KEY_PROV_INFO_PROP_ID);
         if (prov_buf.empty())
             continue;
         CRYPT_KEY_PROV_INFO* provider = (CRYPT_KEY_PROV_INFO*)prov_buf.data();
