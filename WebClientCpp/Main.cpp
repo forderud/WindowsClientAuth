bb52d362ff532fbffcb1dd865ac2c899f0b1ec15
Fredrik Orderud <forderud@gmail.com>
2022-11-07 00:59:40 +0100

    Fix exception catching.

--- WebClientCpp/Main.cpp
+++ WebClientCpp/Main.cpp
@@ -20,54 +20,53 @@ int wmain(int argc, wchar_t* argv[]) {
 
     std::wstring url = L"https://" + hostname;
 
-#ifdef ENABLE_LOW_LEVEL_IMPL
-    CertStore store(L"My", true);
-
-    for (auto it = store.Next(); it; it = store.Next()) {
-        Certificate cert(it);
-        std::wcout << L"Cert: " << cert.Name(CERT_SIMPLE_NAME_STR) << L'\n';
-
-        if (cert.WillExpireInDays(31))
-            std::wcout << L"  Cert will expire within a month.\n";
-
-        std::wcout << L"  Thumbprint: " << cert.ThumbPrintHex() << L'\n';
-
-        std::vector<std::string> ekus = cert.EnhancedKeyUsage(0);
-        for (auto eku : ekus)
-            std::cout << "  EKU: " << eku << '\n';
-
-        CRYPT_KEY_PROV_INFO* priv_key = cert.PrivateKey();
-        if (!priv_key)
-            continue;
-
-        std::wcout << L"  Provider: " << priv_key->pwszProvName << L'\n';
-        std::wcout << L"  Container: " << priv_key->pwszContainerName << L'\n';
-
-        if (priv_key->dwProvType != 0) {
-            std::wcout << L"  Not a CNG type key\n";
-        } else {
-            CNGKey cng(priv_key->pwszProvName, priv_key->pwszContainerName, priv_key->dwKeySpec);
-            cng.Property(NCRYPT_NAME_PROPERTY);
-            std::wcout << L"  CNG key access succeeded\n";
-        }
-
-        auto it2 = std::find(ekus.begin(), ekus.end(), std::string("1.3.6.1.5.5.7.3.2")); // clientAuth OID
-        if (it2 != ekus.end()) {
-            std::wcout << "HTTP request using MSXML6:\n";
-            std::vector<BYTE> thumbprint = cert.ContextProperty(CERT_HASH_PROP_ID);
-            HttpGetMSXML6(url, thumbprint);
-        }
-    }
-#else
     try {
+#ifdef ENABLE_LOW_LEVEL_IMPL
+        CertStore store(L"My", true);
+
+        for (auto it = store.Next(); it; it = store.Next()) {
+            Certificate cert(it);
+            std::wcout << L"Cert: " << cert.Name(CERT_SIMPLE_NAME_STR) << L'\n';
+
+            if (cert.WillExpireInDays(31))
+                std::wcout << L"  Cert will expire within a month.\n";
+
+            std::wcout << L"  Thumbprint: " << cert.ThumbPrintHex() << L'\n';
+
+            std::vector<std::string> ekus = cert.EnhancedKeyUsage(0);
+            for (auto eku : ekus)
+                std::cout << "  EKU: " << eku << '\n';
+
+            CRYPT_KEY_PROV_INFO* priv_key = cert.PrivateKey();
+            if (!priv_key)
+                continue;
+
+            std::wcout << L"  Provider: " << priv_key->pwszProvName << L'\n';
+            std::wcout << L"  Container: " << priv_key->pwszContainerName << L'\n';
+
+            if (priv_key->dwProvType != 0) {
+                std::wcout << L"  Not a CNG type key\n";
+            } else {
+                CNGKey cng(priv_key->pwszProvName, priv_key->pwszContainerName, priv_key->dwKeySpec);
+                cng.Property(NCRYPT_NAME_PROPERTY);
+                std::wcout << L"  CNG key access succeeded\n";
+            }
+
+            auto it2 = std::find(ekus.begin(), ekus.end(), std::string("1.3.6.1.5.5.7.3.2")); // clientAuth OID
+            if (it2 != ekus.end()) {
+                std::wcout << "HTTP request using MSXML6:\n";
+                std::vector<BYTE> thumbprint = cert.ContextProperty(CERT_HASH_PROP_ID);
+                HttpGetMSXML6(url, thumbprint);
+            }
+        }
+#else
         auto clientCert = GetFirstClientAuthCert();
 
         std::wcout << "\n\nHTTP request using WinRT HttpClient:\n";
         HttpGetWinRT(url, clientCert);
-    }
-    catch (hresult_error const& ex) {
+#endif
+    } catch (hresult_error const& ex) {
         std::wcerr << L"ERROR: " << std::wstring(ex.message()) << std::endl;
     }
-#endif
     return 0;
 }
