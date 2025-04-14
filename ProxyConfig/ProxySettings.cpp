be32d65e8f25798ef2b5358e99ea69de51575427
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-04-14 20:49:43 +0200

    .

--- ProxyConfig/ProxySettings.cpp
+++ ProxyConfig/ProxySettings.cpp
@@ -4,8 +4,8 @@
 #pragma comment(lib, "Winhttp.lib")
 
 
-bool PrintProxySettings(const wchar_t* url) {
-    wprintf(L"Checking proxy settings for %s ...\n", url);
+bool PrintProxySettings(const wchar_t* testUrl) {
+    wprintf(L"Checking proxy settings for %s ...\n", testUrl);
 
     WINHTTP_CURRENT_USER_IE_PROXY_CONFIG_wrap settings;
     BOOL ok = WinHttpGetIEProxyConfigForCurrentUser(&settings);
@@ -25,11 +25,11 @@ bool PrintProxySettings(const wchar_t* url) {
         options.fAutoLogonIfChallenged = false;
     }
 
-    if (options.dwFlags & WINHTTP_AUTOPROXY_CONFIG_URL) {
+    if (testUrl && (options.dwFlags & WINHTTP_AUTOPROXY_CONFIG_URL)) {
         HINTERNET_wrap session(WinHttpOpen(/*user agent*/nullptr, WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0));
 
         WINHTTP_PROXY_INFO_wrap info;
-        ok = WinHttpGetProxyForUrl(session, url, &options, &info);
+        ok = WinHttpGetProxyForUrl(session, testUrl, &options, &info);
         if (!ok) {
             DWORD err = GetLastError();
 
@@ -37,7 +37,7 @@ bool PrintProxySettings(const wchar_t* url) {
                 // retry with auto-login enabled
                 // based on https://github.com/chromium/chromium/blob/main/components/winhttp/proxy_configuration.cc
                 options.fAutoLogonIfChallenged = true;
-                ok = WinHttpGetProxyForUrl(session, url, &options, &info);
+                ok = WinHttpGetProxyForUrl(session, testUrl, &options, &info);
                 err = GetLastError();
             }
 
@@ -50,9 +50,8 @@ bool PrintProxySettings(const wchar_t* url) {
 
         info.Print(L"Proxy settings for URL");
         wprintf(L"\n");
-    }
-    else {
-        wprintf(L"Skipping WinHttpGetProxyForUrl lookup, since AutoconfigUrl is disabled\n");
+    } else {
+        wprintf(L"Skipping WinHttpGetProxyForUrl lookup, since AutoconfigUrl is disabled or no url provided\n");
         wprintf(L"\n");
     }
 
