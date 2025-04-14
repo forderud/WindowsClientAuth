a569008d0b8d6b32537202f55ec67f3251acd825
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-04-14 18:38:44 +0200

    Add undocumented autoproxy/setproxy

--- GetProxySettings/Main.cpp
+++ GetProxySettings/Main.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <cassert>
 #include "ProxyStructs.hpp"
+#include <wininet.h>
+#include <vector>
 
 #pragma comment(lib, "Winhttp.lib")
+#pragma comment(lib, "Wininet.lib")
 
 
 bool PrintProxySettings(const wchar_t* url) {
@@ -62,6 +65,67 @@ bool PrintProxySettings(const wchar_t* url) {
 }
 
 
+int UpdateProxySettings(const wchar_t* autoConfigUrl, const wchar_t* proxyServer, const wchar_t* proxyBypass, bool autoDetect) {
+    // bassed on https://www.powershellgallery.com/packages/WinInetProxy/0.1.0/Content/WinInetProxy.psm1
+    wprintf(L"Updating proxy configuration for current user...\n");
+
+    std::vector<INTERNET_PER_CONN_OPTIONW> options(4, INTERNET_PER_CONN_OPTIONW{});
+    {
+        options[0].dwOption = INTERNET_PER_CONN_FLAGS_UI;
+        DWORD& proxyType = options[0].Value.dwValue;
+        proxyType = PROXY_TYPE_DIRECT;
+
+        if (autoDetect)
+            proxyType |= PROXY_TYPE_AUTO_DETECT;
+        wprintf(L"  AutoDetect: %s\n", autoDetect ? L"Enabled" : L"Disabled");
+
+        options[1].dwOption = INTERNET_PER_CONN_PROXY_SERVER;
+        if (proxyServer) {
+            wprintf(L"  Proxy: %s\n", proxyServer);
+            proxyType |= PROXY_TYPE_PROXY;
+            options[1].Value.pszValue = (wchar_t*)proxyServer;
+        }
+
+        options[2].dwOption = INTERNET_PER_CONN_PROXY_BYPASS;
+        if (proxyBypass) {
+            wprintf(L"  ProxyBypass: %s\n", proxyBypass);
+            options[2].Value.pszValue = (wchar_t*)proxyBypass;
+        }
+
+        options[3].dwOption = INTERNET_PER_CONN_AUTOCONFIG_URL;
+        if (autoConfigUrl) {
+            wprintf(L"  AutoconfigUrl: %s\n", autoConfigUrl);
+            proxyType |= PROXY_TYPE_AUTO_PROXY_URL;
+            options[3].Value.pszValue = (wchar_t*)autoConfigUrl;
+        }
+    }
+
+    INTERNET_PER_CONN_OPTION_LISTW list{};
+    list.dwSize = sizeof(list);
+    list.pszConnection = NULL; // LAN
+    list.dwOptionCount = (DWORD)options.size();
+    list.pOptions = options.data();
+
+    HINTERNET session = 0;
+    BOOL ok = InternetSetOptionW(session, INTERNET_OPTION_PER_CONNECTION_OPTION, &list, sizeof(list));
+    if (!ok) {
+        DWORD err = GetLastError();
+        wprintf(L"InternetSetOption INTERNET_OPTION_PER_CONNECTION_OPTION failed with err %u\n", err);
+        abort();
+    }
+
+    ok = InternetSetOptionW(session, INTERNET_OPTION_REFRESH, nullptr, 0);
+    if (!ok) {
+        DWORD err = GetLastError();
+        wprintf(L"InternetSetOption INTERNET_OPTION_REFRESH failed with err %u\n", err);
+        abort();
+    }
+
+    wprintf(L"[completed]\n");
+    return 0;
+}
+
+
 void ProxyChangeCallback(ULONGLONG /*flags*/, void* context) {
     wprintf(L"\n");
     wprintf(L"\n");
@@ -83,6 +147,16 @@ int wmain(int argc, wchar_t* argv[]) {
     }
 
     std::wstring url = argv[1]; // L"http://www.google.com/";
+
+    if ((url == L"autoproxy") && (argc >= 3)) {
+        std::wstring autoConfigUrl = argv[2];
+        return UpdateProxySettings(autoConfigUrl.c_str(), nullptr, nullptr, true);
+    }else if ((url == L"setproxy") && (argc >= 4)) {
+        std::wstring proxy = argv[2];
+        std::wstring bypassList = argv[3];
+        return UpdateProxySettings(nullptr, proxy.c_str(), bypassList.c_str(), true);
+    }
+
 
     PrintProxySettings(url.c_str());
 
